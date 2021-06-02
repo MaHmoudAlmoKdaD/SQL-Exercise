1ca0406be24bf676a6fe1d6eeb98367a7f47fdb2
MaHmoudAlmoKdaD <mahmoudalmokdadtechnology@gmail.com>
2021-06-02 21:11:54 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,23 @@
+0- SELECT * FROM students;
+---------------------------------------
+1- SELECT Name FROM students;
+---------------------------------------
+2- SELECT * FROM students
+WHERE Age > 30;
+---------------------------------------
+3- SELECT * FROM students
+WHERE Age = 30 AND Gender ='F';
+---------------------------------------
+4- SELECT Points FROM students
+WHERE Name = 'Alex';
+---------------------------------------
+5- INSERT INTO students VALUES(7, 'Mahmoud', 27, 'M', 1000);
+---------------------------------------
+6- UPDATE students
+SET Points = 400
+WHERE Name = 'Basma';
+---------------------------------------
+7- UPDATE students
+SET Points = 150
+WHERE Name = 'Alex';
+_______________________________________________________________
